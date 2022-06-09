75b45c1c4ff44741f1f7bc870243f0237a32e659
khumayunkh <hasanovhumayun15@gmail.com>
2022-06-09 21:42:42 +0500

    js

--- inedx.js
+++ inedx.js
@@ -1,75 +1,85 @@
-// console.log('hello')
+// // console.log('hello')
 
-// function sum(a){
-//     let x = 0
-//     for(let i = 0; i<=a; i++){
-//         x+=i
-//     }
-//     return x
+// // function sum(a){
+// //     let x = 0
+// //     for(let i = 0; i<=a; i++){
+// //         x+=i
+// //     }
+// //     return x
+// // }
+
+// // console.log(sum(4)) // 10
+
+// // a = [1,2,3,4,5,6]
+
+// // function santa(){
+// //     let date = new Date()
+// //     if(date.getDate()===11 && date.getDate()===23){
+// //         return true
+// //     }else{
+// //         return false
+// //     }
+// // }
+// // console.log(santa())
+
+
+// let x = [1,12,2,3,4,5]
+// console.log(x.sort())
+// console.log(x.reverse())
+
+document.querySelector('.button').onclick = circuitPower;
+
+function circuitPower(){
+    console.log('hellllo')
+    let a = document.querySelector('.input_1').value;
+    let b = document.querySelector('.input_2').value;
+    console.log('aaaaaaaaaaaaa', a)
+    return document.querySelector('.out').innerHTML = a*b
+}
+
+// function calcAge(a){
+//     return a * 365
 // }
 
-// console.log(sum(4)) // 10
+// console.log(calcAge(2))
 
-// a = [1,2,3,4,5,6]
+// function getFirstValue(a){
+//     return a[0]
+// }
 
-// function santa(){
-//     let date = new Date()
-//     if(date.getDate()===11 && date.getDate()===23){
-//         return true
+// console.log(getFirstValue([1,2,3,4])) // 1
+
+// function findPerimeter(a,b){
+//     return (a+b)*2
+// }
+
+// console.log(findPerimeter(10,20))
+
+
+// function giveMeSomething(a){
+//     return 'something'+' '+a
+// }
+
+// console.log(giveMeSomething('to me'))
+
+// function remainder(a,b){
+//     if(a<b){
+//         return a
 //     }else{
-//         return false
+//         return b
 //     }
 // }
-// console.log(santa())
+// console.log(remainder(4,9)) // 4
 
+// function points(a,b){
+//     return (a*2)+(b*3)
+// }
 
-let x = [1,12,2,3,4,5]
-console.log(x.sort())
-console.log(x.reverse())
+// console.log(points(7,5)) // 29
 
+// function lessThen100(a,b){
+//     return a+b<100 
+// }
 
-function circuitPower(a,b){
-    return a*b
-}
+// console.log(lessThen100(12,38)) // true
 
-console.log(circuitPower(110,3))
-
-function calcAge(a){
-    return a * 365
-}
-
-console.log(calcAge(2))
-
-function getFirstValue(a){
-    return a[0]
-}
-
-console.log(getFirstValue([1,2,3,4]))
-
-function findPerimeter(a,b){
-    return (a+b)*2
-}
-
-console.log(findPerimeter(10,20))
-
-
-function giveMeSomething(a){
-    return 'something'+' '+a
-}
-
-console.log(giveMeSomething('to me'))
-
-function remainder(a,b){
-    if(a<b){
-        return a
-    }else{
-        return b
-    }
-}
-console.log(remainder(4,9)) // 4
-
-function points(a,b){
-    return (a*2)+(b*3)
-}
-
-console.log(points(7,5)) // 29
